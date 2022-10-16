ac1711854d9237fd0193d5571ea370cf0be4a452
Danti_86 <ajux_86@yahoo.com>
2022-10-16 12:57:35 +0100

    Basic Math (Add or Subtract)

---
+++ Kata_7.js
@@ -0,0 +1,9 @@
+// // https://www.codewars.com/kata/5809b62808ad92e31b000031/train/javascript
+// // Basic Math (Add or Subtract)
+// function calculate(str) {
+//     //your code here...
+//     const regex = /plus/ig;
+//     const minus = /minus/ig;
+//     let strSplet = str.replace(regex, "+").replace(minus, "-")
+//     return eval(strSplet); }
+//     console.log(calculate("1plus2plus3minus4"));
